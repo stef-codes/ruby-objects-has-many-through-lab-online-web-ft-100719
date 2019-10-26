316ec033bc920c2f2bdbdbdb728a693ead4e1849
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 02:25:05 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,10 +13,24 @@ class Doctor
       @@all
     end
     
-    def new_appointment(patient,date)
-       Appointment.new(patient,self,date)
-    end
+    # def new_appointment(patient,date)
+    #   Appointment.new(patient,self,date)
+    # end
   
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
 
 end 
