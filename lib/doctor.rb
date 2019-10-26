85224474f327e98aac7037c9fe0431f835b0270e
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 02:29:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select do |appointment|
+    Appointment.all.select do |appointment| 
       appointment.doctor == self
     end
   end
